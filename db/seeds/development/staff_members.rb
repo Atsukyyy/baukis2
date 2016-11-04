57ee6895bec69e82557e9e9dacfbe484a5a037cc
Atsuky <a.mitsuyori.15@gmail.com>
2016-11-04 21:57:27 +0900

    StaffMemberモデルのテーブル作成

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,9 @@
+StaffMember.create!(
+  email: 'atsuki@example.com'
+  family_name: '光頼'
+  given_name: '篤樹'
+  family_name_kana: 'ミツヨリ'
+  given_name_kana: 'アツキ'
+  password: 'password'
+  start_date: Date.today  
+)
