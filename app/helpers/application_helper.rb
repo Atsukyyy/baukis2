6c93b97fdf81d804edcb0f89afa69e1fa7337b03
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2016-11-26 02:22:11 +0900

    form_presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
+module HtmlBuilder
+  def markup(tag_name = nil, options = {})
+    root = Nokogiri::HTML::DocumentFragment.parse('')
+    Nokogiri::HTML::Builder.with(root) do |doc|
+      if tag_name
+        doc.send(tag_name, options) do
+          yield(doc)
+        end
+      else
+        yield(doc)
+      end
+    end
+    root.to_html.html_safe
+  end
+end
+
 module ApplicationHelper
+  include HtmlBuilder
+
   def document_title
     if @title.present?
       "#{@title} - Baukis"
--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,19 +1,3 @@
-module HtmlBuilder
-  def markup(tag_name = nil, options = {})
-    root = Nokogiri::HTML::DocumentFragment.parse('')
-    Nokogiri::HTML::Builder.with(root) do |doc|
-      if tag_name
-        doc.send(tag_name, options) do
-          yield(doc)
-        end
-      else
-        yield(doc)
-      end
-    end
-    root.to_html.html_safe
-  end
-end
-
 class ModelPresenter
   include HtmlBuilder
 
