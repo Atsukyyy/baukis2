91e7da5dce29c5d41bb8798eb0591c0207b0ce26
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2016-11-26 20:24:55 +0900

    customer_form_presenter

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -48,4 +48,12 @@ class FormPresenter
   def decorated_label(name, label_text, options = {})
     label(name, label_text, class: options[:required] ? 'required' : nil)
   end
+
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(name, label_text, options)
+      m << form_builder.select(name, choices, { include_blank: true }, options)
+      #m << error_messages_for(name)
+    end
+  end
 end
