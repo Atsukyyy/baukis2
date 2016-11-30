90f6d615f3478ad94398c6451bb0e000696911bb
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2016-11-30 22:46:17 +0900

    staffmemberformpresenter微修正

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,5 +1,5 @@
 class StaffMemberFormPresenter < UserFormPresenter
-  def check_box
+  def check_boxes
     markup(:div, class: 'input-block') do |m|
       m << check_box(:suspended)
       m << label(:suspended, 'アカウント停止')
